4fd3371e3a877f867ab15fb694dc59b097b0f497
KellenNjoroge <muthonkel@gmail.com>
2018-10-08 09:16:39 +0300

    news api does not support kenya

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class Config:
     TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news,bloomberg,engadget,espn,fortune,al-jazeera-english,cnn,independent&pageSize={}&apiKey={}'
     EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize={}&apiKey={}'
     EVERYTHING_SEARCH_URL = 'https://newsapi.org/v2/everything?q={}&pageSize={}&apiKey={}'
-    KENYA_SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&country=ke&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
